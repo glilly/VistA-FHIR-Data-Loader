7e0b3d1002ba64aecf6930c9d19fd5d99374da9e
Sam Habiel <sam.habiel@gmail.com>
2025-01-16 12:38:45 -0500

    Apply copyright notices to all files
    Apache 2.0 leaves open the possibility of applying copyrights to
    source files, but does not require it. We previously used the NOTICE
    file to disambiguate who has copyright, but since we have lots of
    different copyright holders, I thought it was best to just apply the
    license to each individual file with the correct copyright years.

--- src/SYNKIDS.m
+++ src/SYNKIDS.m
@@ -1,6 +1,20 @@
 SYNKIDS ; OSE/SMH - Synthea Installer ;2019-11-18  5:45 PM
  ;;0.3;VISTA SYNTHETIC DATA LOADER;;Jul 01, 2019;Build 2
  ;
+ ; Copyright (c) 2018-2019 Sam Habiel 
+ ;
+ ;Licensed under the Apache License, Version 2.0 (the "License");
+ ;you may not use this file except in compliance with the License.
+ ;You may obtain a copy of the License at
+ ;
+ ;    http://www.apache.org/licenses/LICENSE-2.0
+ ;
+ ;Unless required by applicable law or agreed to in writing, software
+ ;distributed under the License is distributed on an "AS IS" BASIS,
+ ;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ;See the License for the specific language governing permissions and
+ ;limitations under the License.
+ ;
 ENV ; [Fallthrough]
  QUIT
  ;
